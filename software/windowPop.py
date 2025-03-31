e3e3881fdf43a296e4e114d39900f34a2c8533e0
OMEN-Watson <13521761607@163.com>
2025-03-31 15:55:38 +1100

    add UC node

--- software/windowPop.py
+++ software/windowPop.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # 打包代码：pyinstaller --onefile --windowed --add-data "database.json;." windowPop.py
-
+# C:\Users\GERRY\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\pyinstaller.exe --onefile --windowed --add-data "database.json;." windowPop.py
 # Path to the JSON file
 
 tempPath = "database.json"
